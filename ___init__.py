e47652ccd5bffe8873c07e5b4bc4ba3776b8115b
Rebecca Hudson <rebeccakathrnhudson@gmail.com>
2016-01-28 20:43:13 -0600

    Add the file that makes python treat this diretory as containing a package. So I can include classes from other files. Keep things brief...

---
+++ ___init__.py
@@ -0,0 +1,11 @@
+#  What is this file about??  It is 'blank', I put a comment here as an
+# explanation...
+# see: http://stackoverflow.com/questions/448271/what-is-init-py-for
+#refers to https://docs.python.org/2/tutorial/modules.html#packages,
+#which refers to:  The __init__.py files are required to make Python treat the directories as containing packages
+#
+#
+#
+#
+#
+#
